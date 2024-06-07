7582cd79aa11d2d6e30b2a5b080d785d403be90b
samttoo22 <v99sam@gmail.com>
2024-06-07 11:25:57 +0800

    gui_main updated

--- gui_main.py
+++ gui_main.py
@@ -1,15 +1,26 @@
-import sys
-from time import sleep
-import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
-import auto_download_undetected_chromedriver
-from auto_download_undetected_chromedriver import download_undetected_chromedriver
 import os
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import *
-import tkinter as tk
-from tkinter import ttk
+os.system('python -m pip install undetected_chromedriver')
+os.system('python -m pip install tk')
+try:
+    
+    import sys
+    from time import sleep
+    import undetected_chromedriver as uc
+    from selenium.webdriver.common.by import By
+    
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.common.exceptions import *
+    import tkinter as tk
+    from tkinter import ttk
+    import signal
+    import threading
+    
+    
+except:
+    input("初始化程式中發生錯誤，請按 Enter 關閉程式")
+    sys.exit()
+sys.stderr = open('error_log.txt', 'w')
 
 class NknuSurveyFiller():
     def __init__(self):
@@ -29,26 +40,19 @@ class NknuSurveyFiller():
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--user-data-dir={}".format(os.path.abspath("profile1")))
             return options
-        
-        self.browser_executable_path = ""
-        download_undetected_chromedriver(self.browser_executable_path, undetected=True, arm=False, force_update=True)
+        signal.signal(signal.SIGINT, self.signal_handler)
         self.browser_executable_path = os.path.abspath("chromedriver.exe")
-        
         self.driver = uc.Chrome(options=get_ChromeOptions(), driver_executable_path=self.browser_executable_path, version_main=110)
-        self.driver.get("https://sso.nknu.edu.tw/")
         self.wait = WebDriverWait(self.driver, 10, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
-
-        self.account = ""
-        self.password = ""
         
         self.root = tk.Tk()
         self.root.title("NKNU Survey Filler")
         self.root.geometry("400x300")
-        self.progress_label = ttk.Label(self.root, text="Ready")
+        self.progress_label = ttk.Label(self.root, text="")
         self.progress_label.pack(pady=10)
         self.progress_bar = ttk.Progressbar(self.root, mode='determinate')
         self.progress_bar.pack(pady=10)
-        self.survey_label = ttk.Label(self.root, text="")
+        self.survey_label = ttk.Label(self.root, text="請輸入學號及密碼")
         self.survey_label.pack(pady=10)
         
         self.account_label = ttk.Label(self.root, text="請輸入學號:")
@@ -61,17 +65,40 @@ class NknuSurveyFiller():
         self.password_entry = ttk.Entry(self.root, show="*")
         self.password_entry.pack(pady=5)
         
-        self.start_button = ttk.Button(self.root, text="開始填寫", command=self.start)
-        self.start_button.pack(pady=10)
+        self.start_submit_button = ttk.Button(self.root, text="開始填寫", command=self.start)
+        self.start_submit_button.pack(pady=5)
+        
+        
+        self.account = ""
+        self.password = ""
         
         self.root.mainloop()
+        self.driver.quit()
+        
+    def signal_handler(self, sig, frame):
+        print('You pressed Ctrl+C! Closing ChromeDriver...')
+        self.driver.quit()
+        sys.exit(0)
+
+
+        
         
     def start(self):
-        self.account = self.account_entry.get()
-        self.password = self.password_entry.get()
-        self.login()
-        self.fill_student_survey()
-        self.fill_teacher_survey()
+        print("開始填寫...")
+        self.update_progress("開始填寫...", 5)
+        def _start():
+            self.driver.get("https://sso.nknu.edu.tw/")
+            self.account = self.account_entry.get()
+            self.password = self.password_entry.get()
+            
+            self.login()
+            
+            self.fill_student_survey()
+            self.fill_teacher_survey()
+        
+        threading.Thread(target=_start).start()
+        
+
         
     def update_progress(self, text, value, survey_text=""):
         self.root.after(0, self.progress_label.config, {"text": text})
@@ -82,17 +109,25 @@ class NknuSurveyFiller():
     def login(self):
         self.update_progress("正在登入...", 10)
         print("正在登入...")
+        
         account_block = self.driver.find_element(By.XPATH, "//input[@id='uLoginID']")
         password_block = self.driver.find_element(By.XPATH, "//input[@id='uPassword']")
         remember_button = self.driver.find_element(By.XPATH, "//input[@id='uKeepAlive_ssoLogin']")
         remember_button.click()
         account_block.send_keys(self.account)
         password_block.send_keys(self.password)
-        
         login_button = self.driver.find_element(By.XPATH, "//input[@id='uLoginPassAuthorizationCode']")
         login_button.click()
-        self.update_progress("登入成功", 20)
-        print("登入成功")
+        
+        
+        
+        try:
+            self.wait.until(EC.alert_is_present())
+            self.update_progress("登入失敗，請確認學號及密碼是否正確", 10)
+            sys.exit(0)
+        except TimeoutException:
+            self.update_progress("登入成功", 20)
+            print("登入成功")
     
     def fill_student_survey(self):
         self.update_progress("正在填寫學生問卷...", 30, "學生問卷")
@@ -128,7 +163,9 @@ class NknuSurveyFiller():
                 self.driver.get("https://sso.nknu.edu.tw/StudentProfile/Survey/survey.aspx")
             except Exception as e:
                 self.update_progress("教師問卷填寫完成", 100, "")
+                self.update_progress("問卷填寫完成。", 100, "")
                 print("問卷填寫完成。")
                 sys.exit()
-                
+      
+
 NknuSurveyFiller01 = NknuSurveyFiller()
